d231b395ee594705aab8731a4ced82f0432b0ad8
Felipe Peronica <felipeperonica@gmail.com>
2025-06-24 00:07:13 -0300

    :sparkles: Criação das Páginas de Cadastro e Login

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,25 +1,58 @@
-import { InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import {
+	FacebookLogoIcon,
+	InstagramLogoIcon,
+	LinkedinLogoIcon,
+} from '@phosphor-icons/react'
 
 function Footer() {
+	const data = new Date().getFullYear()
 
-    let data = new Date().getFullYear()
-
-    return (
-        <>
-            <div className="flex justify-center bg-indigo-900 text-white">
-                <div className="container flex flex-col items-center py-4">
-                    <p className='text-xl font-bold'>
-                            Blog Pessoal Felipe Peronica | Copyright: {data}
-                        </p>
-                    <p className='text-lg'>Acesse nossas redes sociais</p>
-                    <div className='flex gap-2'>
-                        <LinkedinLogo size={48} weight='bold' />
-                        <InstagramLogo size={48} weight='bold' />
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+	return (
+		<>
+			<div className="flex justify-center bg-indigo-900 text-white">
+				<div className="container flex flex-col items-center py-4">
+					<p className="text-xl font-bold">
+						Blog Pessoal Generation | Copyright:{' '}
+						{data}
+					</p>
+					<p className="text-lg">
+						Acesse nossas redes sociais
+					</p>
+					<div className="flex gap-2">
+						<div className="flex gap-2">
+							<a
+								href="https://www.linkedin.com/in/felipe-peronica/"
+								target="_blank"
+							>
+								<LinkedinLogoIcon
+									size={48}
+									weight="bold"
+								/>
+							</a>
+							<a
+								href="https://www.instagram.com/seu_usuario"
+								target="_blank"
+							>
+								<InstagramLogoIcon
+									size={48}
+									weight="bold"
+								/>
+							</a>
+							<a
+								href="https://www.facebook.com/seu_usuario"
+								target="_blank"
+							>
+								<FacebookLogoIcon
+									size={48}
+									weight="bold"
+								/>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default Footer
