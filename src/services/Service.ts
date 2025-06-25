679d2d8ee84f67bfe4fb714cb3ccf73b72b11ad4
Felipe Peronica <felipeperonica@gmail.com>
2025-06-25 14:13:11 -0300

    :sparkles: CRUD de Tema

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://blogpessoal-58je.onrender.com",
+  baseURL: "https://blogpessoal-s9ze.onrender.com",
 });
 
 export const cadastrarUsuario = async (
@@ -16,29 +16,15 @@ export const login = async (url: string, dados: Object, setDados: Function) => {
   const resposta = await api.post(url, dados);
   setDados(resposta.data);
 };
-export const buscar = async (
-  url: string,
-  setDados: Function,
-  header: Object
-) => {
+export const buscar = async (url: string, setDados: Function,header: Object) => {
   const resposta = await api.get(url, header);
   setDados(resposta.data);
 };
-export const cadastrar = async (
-  url: string,
-  dados: Object,
-  setDados: Function,
-  header: Object
-) => {
+export const cadastrar = async (url: string, dados: Object, setDados: Function, header: Object) => {
   const resposta = await api.post(url, dados, header);
   setDados(resposta.data);
 };
-export const atualizar = async (
-  url: string,
-  dados: Object,
-  setDados: Function,
-  header: Object
-) => {
+export const atualizar = async (url: string, dados: Object, setDados: Function, header: Object) => {
   const resposta = await api.put(url, dados, header);
   setDados(resposta.data);
 };
