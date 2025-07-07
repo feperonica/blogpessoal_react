00da0745e7f87eb36c3abd65f29de096921822d9
Felipe Peronica <felipeperonica@gmail.com>
2025-07-07 01:00:40 -0300

    :rocket: Deploy do Projeto

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-const api = axios.create({
-    baseURL: "https://blogpessoal-spring-t82.onrender.com",
-    //baseURL: "http://localhost:8080",
-})
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
 
 export const cadastrarUsuario = async (url: string, dados: object, setDados: Function) => {
     const resposta = await api.post(url, dados)
